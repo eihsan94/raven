c56521b37e493c51531d63750cadb53f080f0366
ウンク ムハンマド イサン <bw0021@birdworks.local>
2019-03-24 11:02:57 +0900

    added keypressed feature in drum kits

--- 01_drum_kits/01_drum_kit.js
+++ 01_drum_kits/01_drum_kit.js
@@ -1,16 +1,32 @@
 /* eslint-disable no-unused-vars */
+
+// カーソルからのクリック
 /**
  * Play sound base on the soundName.
- * @param {string} soundName The sound type.
+ * @param {string} keyboardLetter The sound type.
  */
-function playSound(soundName) {
-  const audio = document.getElementById(soundName);
+function playSound(keyboardLetter) {
+  const audio = document.getElementById(keyboardLetter);
   audio.currentTime = 0; // reset the play time
   audio.play();
 }
-// 次回
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key]='${e.keyCode}'`);
-  audio.currentTime = 0; // reset the play time
-  audio.play();
+
+// KEYBOARDからのイベント
+document.addEventListener('keydown', (event) => { // 押すときのイベント
+  const key = (event.key || event.keyCode).toLowerCase();
+  const audio = document.getElementById(key);
+  const keybox = document.getElementById(`${key}-box`);
+  if (audio) {
+    audio.currentTime = 0; // reset the play time
+    audio.play();
+    keybox.classList.add('pressed');
+  }
+});
+document.addEventListener('keyup', (event) => {// 離すときのイベント
+  const key = (event.key || event.keyCode).toLowerCase();
+  const audio = document.getElementById(key);
+  const keybox = document.getElementById(`${key}-box`);
+  if (audio) {
+    keybox.classList.remove('pressed');
+  }
 });
