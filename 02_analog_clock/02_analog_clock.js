e51e2cb7a92bd38d8e75a51b61fb4b930bc2a328
eihsan94 <eihsan94@gmail.com>
2019-03-24 19:20:22 +0900

    02_analog_clock done

--- 02_analog_clock/02_analog_clock.js
+++ 02_analog_clock/02_analog_clock.js
@@ -1,47 +1,89 @@
+/* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
-function clock() {
-  // calculate angle
-  const d = new Date;
-  let h = 30 * ((d.getHours() % 12) + d.getMinutes() / 60);
-  const m = 6 * d.getMinutes();
-  const s = 6 * d.getSeconds();
-  console.log(h);
-  // move hands
-  setAttr('h-hand', h);
-  setAttr('m-hand', m);
-  setAttr('s-hand', s);
-  setAttr('s-tail', s+180);
 
-  if (h !== 12) {
-    h %= 12;
+// use requestAnimationFrame for smoothness (shimmed with setTimeout fallback)
+window.requestAnimFrame = function() {
+  return window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60); // set for 60fps
+  };
+}();
+
+function drawHourTicks() {
+  for (let i = 1; i <= 12; i++) {
+    const svg = document.getElementById('clock-svg'); // Get svg element
+    const el = document.createElementNS('http://www.w3.org/2000/svg', 'line'); // Create a line in SVG's namespace
+    el.setAttribute('x1', '140'); // Set first stroke first x point
+    el.setAttribute('y1', '20'); // Set first stroke first y point
+    el.setAttribute('x2', '140'); // Set first stroke second x point
+    el.setAttribute('y2', '40'); // Set first stroke second x point
+    el.setAttribute('id', 'tick');
+    el.setAttribute('transform', 'rotate(' + (i*360/12) + ' 140 140)');
+    svg.appendChild(el);
   }
+}
 
-  // call every second
-  setTimeout(clock, 1000);
-};
+function clockTicking() {
+  // now
+  const now = new Date();
+  // midnight
+  const then = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
+  const d = new Date;
+  const h = 30 * ((d.getHours() % 12) + d.getMinutes() / 60);
+  const diffInMil = (now.getTime() - then.getTime());// difference in milliseconds
+  const hours = (diffInMil/(1000*60*60)); // hours
+  const m = (hours * 60); // minutes
+  const s = (m * 60); // seconds
+  // move clock hands
+  setAttr('h-hand', h);
+  setAttr('m-hand', m*6);
+  setAttr('s-hand', s*6);
+  setAttr('s-tail', (s*6)+180);
+}
 
 function setAttr(id, val) {
   const v = 'rotate(' + val + ', 140, 140)';
   document.getElementById(id).setAttribute('transform', v);
-};
+}
 
 // 全てのウェブ部分が表示された時に動かす初期化の関数
 document.addEventListener('DOMContentLoaded', function() {
   console.log('All assets are loaded');
-  clock();
+  drawHourTicks();
+  (function loop() {
+    requestAnimFrame(loop);
+    clockTicking();
+  })();
 });
 
-let x;
-let y;
-const rad = 140;
-const divisions = 12;
-const degreesPerIter = 360 / divisions;
-const startAngleDeg = 90;
-for (i = 1; i < divisions + 1; i++) {
-  const angleDeg = (startAngleDeg + (i * degreesPerIter)) % 360;
 
-  x = rad * Math.cos(angleDeg * Math.PI/180);
-  console.log((angleDeg));
-  y = rad * Math.sin(angleDeg * Math.PI/180);
-  console.log(`coordinates of the 12 o'clock mark are ${i} o'clock (${x}, ${y}) `);
-}
+// function clock() {
+//   // calculate angle
+//   const d = new Date;
+//   let h = 30 * ((d.getHours() % 12) + d.getMinutes() / 60);
+//   const m = 6 * d.getMinutes();
+//   const s = 6 * d.getSeconds();
+//   // move hands
+//   setAttr('h-hand', h);
+//   // console.log(h);
+
+//   // setAttr('m-hand', m);
+//   // setAttr('s-hand', s);
+//   // setAttr('s-tail', s+180);
+
+//   if (h !== 12) {
+//     h %= 12;
+//   }
+
+//   // call every second
+//   setTimeout(clock, 1000);
+// }
+
+// // 全てのウェブ部分が表示された時に動かす初期化の関数
+// document.addEventListener('DOMContentLoaded', function() {
+//   console.log('All assets are loaded');
+//   clock();
+//   hourTick();
+// });
