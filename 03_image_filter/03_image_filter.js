6979113e15bd890d3a4c8409e4a7d760c97a213e
ウンク ムハンマド イサン <bw0021@birdworks.local>
2019-03-31 12:51:18 +0900

    03_image_filter done

---
+++ 03_image_filter/03_image_filter.js
@@ -0,0 +1,22 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+
+function colorChange(event) {
+  const imageContainerElement = document.querySelector('.image-container');
+  const currentColor = event.srcElement.value;
+  imageContainerElement.style.backgroundColor = currentColor;
+}
+function blurChange(event) {
+  const imageElement = document.querySelector('.image');
+  currentBlurValue = event.srcElement.value;
+  console.log(`blur(${currentBlurValue})`);
+  imageElement.style.filter = `blur(${currentBlurValue - 1}px)`;
+  imageElement.style.WebkitFilter = `blur(${currentBlurValue - 1}px)`;
+}
+
+function spacingChange(event) {
+  const imageElement = document.querySelector('.image');
+  const currentSpace = event.srcElement.value;
+  imageElement.style.marginLeft = `${currentSpace}px`;
+  imageElement.style.marginTop = `${currentSpace}px`;
+}
